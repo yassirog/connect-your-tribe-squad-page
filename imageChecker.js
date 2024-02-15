78a59907273e1b68b677b508cd3ab4e2db24ea5b
yassir bghiel <yassir.bghiel@hva.nl>
2024-02-15 23:49:50 +0100

    fout

--- imageChecker.js
+++
@@ -1,8 +0,0 @@
-function isImgUrl(url) {
-    const img = new Image();
-    img.src = url;
-    return new Promise((resolve) => {
-    img.onerror = () => resolve(false);
-    img.onload = () => resolve(true);
-    });
-}
