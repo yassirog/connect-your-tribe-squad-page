988da6a44116505b203c1881e0248d6d066d6566
yassir bghiel <yassir.bghiel@hva.nl>
2024-03-01 02:00:19 +0100

    final push

--- public/script/script.js
+++ public/script/script.js
@@ -1,3 +1,4 @@
+// Slider
 const slider = document.querySelector(".slider");
 const prevBtn = document.querySelector(".prevBtn");
 const nextBtn = document.querySelector(".nextBtn");
@@ -5,6 +6,8 @@ const nextBtn = document.querySelector(".nextBtn");
 prevBtn.addEventListener('click', slideLeft);
 nextBtn.addEventListener('click', slideRight);
 
+
+// Je bekijkt de breedte van de card-item nadat je dat berekent scroll je naar links/rechts elke keer met die breedte.
 function slideLeft() {
     const itemWidth = slider.querySelector('.card-item').offsetWidth;
     const scrollAmount = slider.scrollLeft - itemWidth;
